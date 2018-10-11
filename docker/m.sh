8961c66af10fb86ccd579e092aba3cad6ccd0904
Jan Schiefer <jschiefer@users.noreply.github.com>
2018-10-11 00:13:47 -0700

    Workable docker container with gnuradio 3.7.13.4, gr-limesdr, gr-iio

--- docker/m.sh
+++ docker/m.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm --privileged -e USER=amsat -v ~/Docker:/home/amsat/Docker molino-gnuradio:0.2 /bin/bash
-
+docker run -it --rm --privileged -e USER=amsat --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" -v ~/Docker:/home/amsat/Docker -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v /dev/bus/usb:/dev/bus/usb gnuradio:0.1 /bin/bash
--- docker/sc.sh
+++ docker/sc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -it --rm --privileged -e USER=amsat -v ~/Docker:/home/amsat/Docker molino-lime:0.1 /bin/bash
+docker run -it --rm --privileged -e USER=amsat -v ~/Docker:/home/amsat/Docker gnuradio:0.1 /bin/bash
 
